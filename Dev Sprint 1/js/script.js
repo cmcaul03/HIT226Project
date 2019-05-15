a3d8bcf7fe0d82f260e83157b707eaa9a58b4baa
cmcaul03 <cameron.mcauliffe7@gmail.com>
2019-05-15 20:32:42 +1000

    Added back to top button

--- Dev Sprint 1/js/script.js
+++ Dev Sprint 1/js/script.js
@@ -18,3 +18,20 @@ $(document).ready(function() {
 function is_touch_device() {
   return !!('ontouchstart' in window);
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topbutton").style.display = "block";
+  } else {
+    document.getElementById("topbutton").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
